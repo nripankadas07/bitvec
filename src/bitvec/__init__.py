d3ff27ba29b10ca598efd984641e6a3cb6d73633
Dr Nripanka Das <nripankadas@gmail.com>
2026-04-20 20:04:27 +0400

    Add src/bitvec/__init__.py

---
+++ src/bitvec/__init__.py
@@ -0,0 +1,6 @@
+"""BitVec: compact bit vector with set operations and slicing."""
+from bitvec.core import BitVec
+from bitvec.errors import BitVecError
+
+__all__ = ['BitVec', 'BitVecError']
+__version__ = '0.1.0'
